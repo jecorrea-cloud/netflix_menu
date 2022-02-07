c9f368f9d895731de79188eb76a4c7c3aa8fd94d
joaquin <jecorrea@protonmail.com>
2022-02-06 19:10:30 -0500

    Completed buttons functionality

--- script.js
+++ script.js
@@ -5,3 +5,11 @@ const close_btn = document.querySelector(".close-btn");
 
 // ALL the navs
 const nav = document.querySelectorAll(".nav");
+
+open_btn.addEventListener("click", () => {
+  nav.forEach((nav_el) => nav_el.classList.add("visible"));
+});
+
+close_btn.addEventListener("click", () => {
+  nav.forEach((nav_el) => nav_el.classList.remove("visible"));
+});
