2ea77e8abe0c552dab5095a2e843b6391d01eb08
joaquin <jecorrea@protonmail.com>
2022-02-06 19:06:08 -0500

    initial JS

---
+++ script.js
@@ -0,0 +1,7 @@
+// Bring down the elements
+// Buttons
+const open_btn = document.querySelector(".open-btn");
+const close_btn = document.querySelector(".close-btn");
+
+// ALL the navs
+const nav = document.querySelectorAll(".nav");
